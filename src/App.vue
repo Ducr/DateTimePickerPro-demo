cc210cb7de0809fdfb5cafb242d971bbee14aa1a
Ducr <ducrong@126.com>
2025-11-03 18:05:02 +0800

    feat: 安装引入使用date-time-picker-pro

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,153 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 style="margin-bottom: 50px">DateTimePickerPro Demo</h1>
+    <div style="margin-bottom: 50px">
+      el-time-picker：
+      <el-time-picker
+        v-model="singleTimeEl"
+        placeholder="请选择时间"
+        :custom-minute-step="20"
+        :custom-second-step="10"
+      />
+      <el-time-picker
+        v-model="multipleTimeEl"
+        is-range
+        range-separator="至"
+        start-placeholder="开始时间"
+        end-placeholder="结束时间"
+        placeholder="选择时间范围"
+      />
+    </div>
+    <div style="margin-bottom: 50px">
+      date-time-picker-pro（无步距）：
+      <!-- 单个日期时间 -->
+      <date-time-picker-pro
+        v-model="singleDateTime"
+        type="datetime"
+        placeholder="选择日期时间"
+        default-time="06:00:00"
+        value-format="yyyy/MM/dd HH:mm:ss"
+        format="yyyy/MM/dd HH:mm:ss"
+      />
+      <!-- 日期时间范围 -->
+      <date-time-picker-pro
+        v-model="multipleDateTime"
+        type="datetimerange"
+        :default-time="['09:00:00', '19:00:00']"
+        range-separator="至"
+        start-placeholder="开始日期"
+        end-placeholder="结束日期"
+        value-format="yyyy/MM/dd HH:mm:ss"
+        format="yyyy/MM/dd HH:mm:ss"
+      />
+    </div>
+    <div style="margin-bottom: 50px">
+      date-time-picker-pro（自定义步距）：
+      <!-- 单个日期时间 -->
+      <date-time-picker-pro
+        v-model="singleDateTime1"
+        type="datetime"
+        placeholder="选择日期时间"
+        default-time="06:00:00"
+        :custom-minute-step="20"
+        :custom-second-step="10"
+        value-format="yyyy/MM/dd HH:mm:ss"
+        format="yyyy/MM/dd HH:mm:ss"
+      />
+      <!-- 日期时间范围 -->
+      <date-time-picker-pro
+        v-model="multipleDateTime1"
+        type="datetimerange"
+        :default-time="['09:00:00', '19:00:00']"
+        :custom-minute-step="30"
+        :custom-second-step="15"
+        range-separator="至"
+        start-placeholder="开始日期"
+        end-placeholder="结束日期"
+        value-format="yyyy/MM/dd HH:mm:ss"
+        format="yyyy/MM/dd HH:mm:ss"
+      />
+    </div>
+    <!-- <div style="margin-bottom: 50px">
+      time-picker-pro（无步距）：
+      <time-picker-pro
+        v-model="singleTime"
+        placeholder="请选择时间"
+      />
+      <time-picker-pro
+        v-model="multipleTime"
+        is-range
+        range-separator="至"
+        start-placeholder="开始时间"
+        end-placeholder="结束时间"
+        placeholder="选择时间范围"
+      />
+    </div>
+    <div style="margin-bottom: 50px">
+      time-picker-pro（自定义步距+使用滚动）：
+      <time-picker-pro
+        v-model="singleTime1"
+        placeholder="请选择时间"
+        :custom-minute-step="20"
+        :custom-second-step="10"
+      />
+      <time-picker-pro
+        v-model="multipleTime1"
+        is-range
+        :custom-minute-step="20"
+        :custom-second-step="10"
+        range-separator="至"
+        start-placeholder="开始时间"
+        end-placeholder="结束时间"
+        placeholder="选择时间范围"
+      />
+    </div>
+    <div style="margin-bottom: 50px">
+      time-picker-pro（自定义步距+使用箭头）：
+      <time-picker-pro
+        v-model="singleTime2"
+        arrow-control
+        placeholder="请选择时间"
+        :custom-minute-step="20"
+        :custom-second-step="10"
+      />
+      <time-picker-pro
+        v-model="multipleTime2"
+        is-range
+        arrow-control
+        :custom-minute-step="20"
+        :custom-second-step="10"
+        range-separator="至"
+        start-placeholder="开始时间"
+        end-placeholder="结束时间"
+        placeholder="选择时间范围"
+      />
+    </div> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: {},
+  data() {
+    return {
+      singleTimeEl: '',
+      multipleTimeEl: [new Date(2025, 10, 31, 8, 40), new Date(2025, 10, 30, 18, 40)],
+      singleDateTime: '',
+      multipleDateTime: [],
+      singleDateTime1: '',
+      multipleDateTime1: [],
+      singleTime: '',
+      multipleTime: [],
+      singleTime1: '',
+      multipleTime1: [],
+      singleTime2: '',
+      multipleTime2: [],
+    };
+  },
+  methods: {},
+};
 </script>
 
 <style>
@@ -24,5 +158,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.el-date-editor + .el-date-editor {
+  margin-left: 20px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import DateTimePickerPro from 'date-time-picker-pro'
+
+Vue.use(ElementUI)
+Vue.use(DateTimePickerPro)
 
 Vue.config.productionTip = false
 
